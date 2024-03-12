908833d0db3d58de1b2ab022a275d8e0aa5dea17
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-03-12 14:18:17 +0600

    hashpassword used

--- Backend/controllers/signupController.js
+++ Backend/controllers/signupController.js
@@ -1,26 +1,35 @@
 const userModel = require("../models/userModel.js");
+const bcryptjs = require("bcryptjs"); //here we bcryptjs instead of bcrypt because it's gives problem's during deployment
 const signup = async (req, res) => {
-  console.log(req.body);
-  console.log(req.data);
-  const { username, email, password } = req.body;
-
-  if (
-    !username ||
-    !email ||
-    !password ||
-    username === "" ||
-    email === "" ||
-    password === ""
-  ) {
-    return res.status(400).json({ message: "All fields are required" });
-  } else {
-    const newUser = new userModel({
-      username,
-      email,
-      password,
-    });
-    await newUser.save();
-    res.status(200).send({ message: "Signed up successfully" });
+  try {
+    const { username, email, password } = req.body;
+    if (
+      !username ||
+      !email ||
+      !password ||
+      username === "" ||
+      email === "" ||
+      password === ""
+    ) {
+      return res.status(400).json({ message: "All fields are required" });
+    } else {
+      const existingUser = await userModel.findOne({ email: email });
+      if (existingUser) {
+        res.status(409).send("Email already in use");
+      } else {
+        // hash the password before saving to database
+        const hashPassword = bcryptjs.hashSync(password, 10);
+        const newUser = new userModel({
+          username,
+          email,
+          password: hashPassword,
+        });
+        await newUser.save();
+        res.status(200).send({ message: "Signed up successfully" });
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
