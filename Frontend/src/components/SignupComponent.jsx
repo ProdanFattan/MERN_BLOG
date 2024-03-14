9bd6fb662c59a15b54a21e7e89341489901dad32
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-03-14 23:18:22 +0600

    signup funtionalitiy

--- Frontend/src/components/SignupComponent.jsx
+++ Frontend/src/components/SignupComponent.jsx
@@ -1,19 +1,43 @@
-import React from "react";
-import { Button } from "flowbite-react";
+import React, { useState } from "react";
+import { Alert, Button, Spinner } from "flowbite-react";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Input } from "../components/index";
 
 const SignupComponent = () => {
   const { register, handleSubmit } = useForm();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const createAccount = async (formData) => {
+    try {
+      setError("");
+      setLoading(true);
+      const res = await fetch("/api/v1/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setError(data.message);
+      } else {
+        navigate("/");
+      }
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(createAccount)}>
         <div className="space-y-5">
           <Input
             label="Full Name: "
             placeholder="Enter your full name"
-            {...register("name", {
+            {...register("username", {
               required: true,
             })}
           />
@@ -42,8 +66,16 @@ const SignupComponent = () => {
             type="submit"
             className="w-full"
             gradientDuoTone="purpleToPink"
+            disabled={loading}
           >
-            Create Account
+            {loading ? (
+              <>
+                <Spinner size="sm" />
+                <span className="pl-3">Creating Account</span>
+              </>
+            ) : (
+              "Create Account"
+            )}
           </Button>
         </div>
       </form>
@@ -53,6 +85,11 @@ const SignupComponent = () => {
           Sign In
         </Link>
       </div>
+      {error && (
+        <Alert className="mt-5" color="failure">
+          {error}
+        </Alert>
+      )}
     </>
   );
 };
