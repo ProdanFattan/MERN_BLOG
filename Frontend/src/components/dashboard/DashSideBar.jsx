95a3e4be163ab1f3fdf7cd8eb7e7d8cd937584ae
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-03-16 12:24:14 +0600

    Dashboard comps create

---
+++ Frontend/src/components/dashboard/DashSideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashSideBar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DashSideBar;
--- Frontend/src/components/index.js
+++ Frontend/src/components/index.js
@@ -5,6 +5,13 @@ import SignupComponent from "./SignupComponent";
 import SigninComponent from "./SigninComponent";
 import GoogleOauth from "./GoogleOauth";
 import ProtectedRoute from "./ProtectedRoute";
+import DashboardComp from "./dashboard/DashboardComp";
+import DashSideBar from "./dashboard/DashSideBar";
+import DashComments from "./dashboard/DashComments";
+import DashPosts from "./dashboard/DashPosts";
+import DashProfile from "./dashboard/DashProfile";
+import DashUsers from "./dashboard/DashUsers";
+
 export {
   Header,
   Footer,
@@ -13,4 +20,10 @@ export {
   SigninComponent,
   GoogleOauth,
   ProtectedRoute,
+  DashUsers,
+  DashProfile,
+  DashPosts,
+  DashComments,
+  DashSideBar,
+  DashboardComp,
 };
